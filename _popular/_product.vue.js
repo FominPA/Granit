9b2df523c26eed8fd8f7cb341d2ceff9d9d86b70
Pavel Fomin <webdev.fominpa@gmail.com>
2021-08-09 17:36:43 +0400

    realize computed for bgImage prop of index _popular__product

--- _popular/_product.vue.js
+++ _popular/_product.vue.js
@@ -1,7 +1,11 @@
 import { yellowButton } from '../_button/_button.vue.js';
 
 export const popularProduct = {
-	props: ['bgUrl', 'hrefBuy', 'title', 'sale', 'basicPrice', 'oldPrice', 'hotPrice'],
+	props: ['imageUrl', 'hrefBuy', 'title', 'sale', 'basicPrice', 'oldPrice', 'hotPrice'],
+
+	computed: {
+		bgImageStyle() { return ('background-image: url(' + this.imageUrl + ');') }
+	},
 
 	components: {
 		'yellow-button': yellowButton,
@@ -9,7 +13,7 @@ export const popularProduct = {
 
 	template:
 	`<div class="popular__item">
-		<div class="popular__item-image" :style='["background-image: url(" + bgUrl + ")"]'><h3 v-if='sale' class="popular__item-sale">Акция</h3></div>
+		<div class="popular__item-image" :style='bgImageStyle'><h3 v-if='sale' class="popular__item-sale">Акция</h3></div>
 		<h3 class="popular__item-title">{{ title }}</h3>
 		<div class="popular__item-price">
 			<h3 v-if='!sale' class='popular__item-price_basic-price'>{{ basicPrice }}</h3>
